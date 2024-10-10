74b237fbd300c22ebaa21013c224308f1936d366
bounceslimeman <165736133+bounceslimeman@users.noreply.github.com>
2024-10-10 10:17:26 -0500

    Code Base 1 with Comments

--- WrittenCodeSetOne.py
+++ WrittenCodeSetOne.py
@@ -8,3 +8,9 @@ Created on Sun Sep 29 16:41:39 2024
 """
 This is a comment line meant to test the first github push of the code confirming the repository is working correctly 
 """
+import adi
+import time
+import matplotlib.pyplot as plt
+import numpy as np
+#this is the first set of imports
+
